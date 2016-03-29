6e21d0dd59641f299c79af69961b484088a56c1c
cwaw72 <cwaw@naver.com>
2016-03-29 15:31:04 +0900

    done

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     TextView in1 ;
     TextView in2 ;
     TextView result ;
+    TextView calculator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,17 +25,17 @@ public class MainActivity extends AppCompatActivity {
         in1 =  (TextView) findViewById(R.id.input1);
         in2 =  (TextView) findViewById(R.id.input2);
         result =  (TextView) findViewById(R.id.result);
+        calculator = (TextView) findViewById(R.id.calculator);
     }
 
     public void CButton(View v) {
 
         switch (v.getId()) {
             case R.id.num0:
-                if(in1.getTextSize() != 0 || in2.getTextSize() != 0 )
                     if(next == false)
-                        in1.append("0");
+                        if(in1.getText().length() != 0 ) in1.append("0");
                     else
-                        in2.append("0");
+                        if(in2.getText().length() != 0 ) in2.append("0");
                 break;
             case R.id.num1:
                 if(next == false)
@@ -92,24 +94,26 @@ public class MainActivity extends AppCompatActivity {
             case R.id.add:
                 next = true;
                 cal = 0;
+                calculator.setText("Add");
                 break;
             case R.id.subtract:
                 next = true;
                 cal = 1;
+                calculator.setText("Subtract");
                 break;
 
             case R.id.multiply:
                 next = true;
                 cal = 2;
+                calculator.setText("Multiply");
                 break;
 
             case R.id.divide:
                 next = true;
                 cal = 3;
+                calculator.setText("Divide");
                 break;
             case R.id.one :
-                Log.e("","ab"+ in1.getTextSize());
-
                 next = false;
                 break;
             case R.id.two :
@@ -121,14 +125,15 @@ public class MainActivity extends AppCompatActivity {
                 in2.setText("");
                 result.setText("");
                 cal=0;
+                calculator.setText("Add");
                 break;
             case R.id.resultButton:
                 int n1 = Integer.parseInt(in1.getText().toString());
                 int n2 = Integer.parseInt(in2.getText().toString());
                 if(cal == 0)  result.setText( "Result : " + (n1 + n2)  );
                 else if(cal == 1)  result.setText("Result : "+ (n1 - n2));
-                else if(cal == 2)  result.setText("Result : "+ (n1 / n2));
-                else  result.setText("Result : "+ (n1 * n2));
+                else if(cal == 2)  result.setText("Result : "+ (n1 * n2));
+                else  result.setText("Result : "+ (n1 / n2));
         }
     }
 }
